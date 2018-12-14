4b64845c980c80dc4fd1ab1bd8b9681940335de1
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:08:36 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/dmx_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your DMX Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	dmx, _ := dmx_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Configure Bricklet as DMX master
+	dmx.SetDMXMode(dmx_bricklet.DMXModeMaster)
+
+	// Write DMX frame with 3 channels
+	dmx.WriteFrame([]uint8{255, 128, 0})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
