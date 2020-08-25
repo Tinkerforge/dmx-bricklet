5c887cceb0fd42822c529982a6397dd1279963a8
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:51:51 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_dmx.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your DMX Bricklet
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_DMX dmx;
 
