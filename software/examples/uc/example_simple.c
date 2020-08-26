58e84a86adf7179f6b0c0d7a677cba3577d6c0cd
Erik Fleckstein <erik@tinkerforge.com>
2020-08-26 10:35:26 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -11,14 +11,12 @@ void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_dmx_create(&dmx, UID, hal), "create device object");
 
-
 	// Configure Bricklet as DMX master
 	check(tf_dmx_set_dmx_mode(&dmx, TF_DMX_DMX_MODE_MASTER), "call set_dmx_mode");
 
 	// Write DMX frame with 3 channels
 	uint8_t frame[3] = {255, 128, 0};
 	check(tf_dmx_write_frame(&dmx, frame, 3), "call write_frame");
-
 }
 
 void example_loop(TF_HalContext *hal) {
