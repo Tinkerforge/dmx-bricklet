43ac6167a6c5621d2201a04f7acaf53059100fe7
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 11:40:37 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_dmx.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_dmx.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your DMX Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_DMX dmx;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_dmx_create(&dmx, UID, hal), "create device object");
+	check(tf_dmx_create(&dmx, NULL, hal), "create device object");
 
 	// Configure Bricklet as DMX master
 	check(tf_dmx_set_dmx_mode(&dmx, TF_DMX_DMX_MODE_MASTER), "call set_dmx_mode");
